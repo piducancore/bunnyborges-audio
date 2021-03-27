38c5f4345d8b6edd589447a88ddc430892a0f672
Rodrigo Moreira <hola@piducan.dev>
2021-03-27 01:48:07 -0300

    add sample bot code

--- api/index.js
+++ api/index.js
@@ -1,7 +1,43 @@
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  });
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.TELEGRAM_API_TOKEN);
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on("sticker", (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.on("inline_query", async (ctx) => {
+  const apiUrl = `http://recipepuppy.com/api/?q=${ctx.inlineQuery.query}`;
+  const response = await fetch(apiUrl);
+  const { results } = await response.json();
+  const recipes = results
+    .filter(({ thumbnail }) => thumbnail)
+    .map(({ title, href, thumbnail }) => ({
+      type: "article",
+      id: thumbnail,
+      title: title,
+      description: title,
+      thumb_url: thumbnail,
+      input_message_content: {
+        message_text: title,
+      },
+      reply_markup: Markup.inlineKeyboard([Markup.button.url("Go to recipe", href)]),
+    }));
+  return await ctx.answerInlineQuery(recipes);
+});
+
+bot.on("chosen_inline_result", ({ chosenInlineResult }) => {
+  console.log("chosen inline result", chosenInlineResult);
+});
+bot.launch();
+
+module.exports = async (req, res) => {
+  try {
+    await bot.handleUpdate(req.body);
+  } finally {
+    res.json({
+      body: req.body,
+      query: req.query,
+      cookies: req.cookies,
+    });
+  }
 };
