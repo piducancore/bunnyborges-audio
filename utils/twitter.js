a32ee2f1caca0d73c0ec421d63ae2bf684c27ed7
Rodrigo Moreira <hola@piducan.dev>
2021-04-08 17:52:44 -0400

    use search

--- utils/twitter.js
+++ utils/twitter.js
@@ -1,26 +1,26 @@
 const Twitter = require("twitter-lite");
 
 async function getClient() {
-  const auth = new Twitter({
+  return new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  });
-  const { access_token } = await auth.getBearerToken();
-  return new Twitter({
-    bearer_token: access_token,
+    access_token_key: "",
+    access_token_secret: "",
   });
 }
 
-async function getLastTweet(user) {
+async function getLastest(user) {
   const twitter = await getClient();
-  const [lastTweet] = await twitter.get("statuses/user_timeline", {
-    screen_name: user,
-    exclude_replies: true,
+  const { statuses } = await twitter.get("search/tweets", {
+    q: `from:${user} -is:retweet`,
+    count: 100,
+    include_entities: false,
+    result_type: "recent",
   });
-  return lastTweet;
+  return statuses.map((status) => status.text.replace(/^@[A-Za-z0-9]+\ /, ""));
 }
 
 module.exports = {
   getClient,
-  getLastTweet,
+  getLastest,
 };
